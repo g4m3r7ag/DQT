32e1089e9327f96e8d6150f07be5ff4fc875610e
g4m3r7ag <g4m3r7ag@gmail.com>
2022-02-20 23:56:00 -0500

    updated ru/de/fr to be consistent with en, updated ui dimensions, new sections default to shown

--- PolloxsDailyQuestTracker/lang/en.lua
+++ PolloxsDailyQuestTracker/lang/en.lua
@@ -9,41 +9,41 @@ L.SI_DQT_SECTION_HEADER				= "Sections to Show"
 
 -- Section Names
 L.SI_DQT_CRAFTING					= GetString(SI_QUESTTYPE4)
-L.SI_DQT_UNDAUNTED_PLEDGE			= "Undaunted Pledges"
-L.SI_DQT_SUMMERSET					= "Summerset"
-L.SI_DQT_VVARDENFELL				= "Vvardenfell"
+L.SI_DQT_UNDAUNTED_PLEDGE			= GetString(SI_QUESTTYPE15)
+L.SI_DQT_SUMMERSET					= GetZoneNameById(1011)
+L.SI_DQT_VVARDENFELL				= GetZoneNameById(849)
 L.SI_DQT_GUILD						= GetString(SI_QUESTTYPE3)
 L.SI_DQT_CYRODILIC_COLLECTIONS		= "Cyrodilic Collections"
-L.SI_DQT_CLOCKWORK_CITY				= "Clockwork City"
-L.SI_DQT_WROTHGAR					= "Wrothgar"
-L.SI_DQT_THIEVES_GUILD				= "Thieves Guild"
-L.SI_DQT_DARK_BROTHERHOOD			= "Dark Brotherhood"
-L.SI_DQT_MURKMIRE					= "Murkmire"
+L.SI_DQT_CLOCKWORK_CITY				= GetZoneNameById(980)
+L.SI_DQT_WROTHGAR					= GetZoneNameById(684)
+L.SI_DQT_THIEVES_GUILD				= GetZoneNameById(816)
+L.SI_DQT_DARK_BROTHERHOOD			= GetZoneNameById(823)
+L.SI_DQT_MURKMIRE					= GetZoneNameById(726)
 L.SI_DQT_OTHER_TIMERS				= "Other Timers"
-L.SI_DQT_RANDOM_DUNGEON				= "Random Dungeon"
-L.SI_DQT_RANDOM_BATTLEGROUNDS		= "Random Battlegrounds"
-L.SI_DQT_MOUNT_TRAINING				= "Mount Training"
-L.SI_DQT_BEQUEATHER					= "Shadowy Supplier"
+L.SI_DQT_RANDOM_DUNGEON				= GetString(SI_DUNGEON_FINDER_RANDOM_FILTER_TEXT)
+L.SI_DQT_RANDOM_BATTLEGROUNDS		= GetString(SI_BATTLEGROUND_FINDER_RANDOM_FILTER_TEXT)
+L.SI_DQT_MOUNT_TRAINING				= GetString(SI_STAT_GAMEPAD_RIDING_HEADER_TRAINING)
+L.SI_DQT_BEQUEATHER					= select(1, GetSkillAbilityInfo(5,1,4))
 L.SI_DQT_ELSWEYR_PROLOGUE			= "Elsweyr Prologue"
-L.SI_DQT_ELSWEYR					= "Elsweyr"
-L.SI_DQT_DRAGONHOLD					= "Dragonhold" -- Added by DarkPhalanx
-L.SI_DQT_WESTERN_SKYRIM				= "Western Skyrim" -- Added by DarkPhalanx
-L.SI_DQT_IMPERIAL_CITY				= "Imperial City" -- Added by DarkPhalanx
-L.SI_DQT_THE_REACH					= "The Reach" -- Added by DarkPhalanx
+L.SI_DQT_ELSWEYR					= GetZoneNameById(1086)
+L.SI_DQT_DRAGONHOLD					= GetZoneNameById(1133) -- Added by DarkPhalanx
+L.SI_DQT_WESTERN_SKYRIM				= GetZoneNameById(1160) -- Added by DarkPhalanx
+L.SI_DQT_IMPERIAL_CITY				= GetZoneNameById(584) -- Added by DarkPhalanx
+L.SI_DQT_THE_REACH					= GetZoneNameById(1207) -- Added by DarkPhalanx
 L.SI_DQT_LOWER_CRAGLORN				= "Lower Craglorn" -- Added by DarkPhalanx
 L.SI_DQT_UPPER_CRAGLORN				= "Upper Craglorn" -- Added by DarkPhalanx
-L.SI_DQT_BLACKWOOD					= "Blackwood" -- Added by DarkPhalanx
-L.SI_DQT_DEADLANDS					= "Deadlands" -- Added by g4m3r7ag
+L.SI_DQT_BLACKWOOD					= GetZoneNameById(1261) -- Added by DarkPhalanx
+L.SI_DQT_DEADLANDS					= GetZoneNameById(1286) -- Added by g4m3r7ag
 
 -- Quest Type Names
-L.SI_DQT_GROUP_BOSS					= "Group Boss"
+L.SI_DQT_GROUP_BOSS					= GetString(SI_ZONECOMPLETIONTYPE9)
 L.SI_DQT_DELVE						= GetString(SI_INSTANCEDISPLAYTYPE7)
 L.SI_DQT_GEYSERS					= "Geysers"
 L.SI_DQT_ASHLANDER_HUNT				= "Ashlander Hunt"
 L.SI_DQT_ASHLANDER_RELIC			= "Ashlander Relic"
-L.SI_DQT_FIGHTERS_GUILD				= "Fighters Guild"
-L.SI_DQT_MAGES_GUILD				= "Mages Guild"
-L.SI_DQT_UNDAUNTED_DELVE			= "Undaunted Delve"
+L.SI_DQT_FIGHTERS_GUILD				= GetSkillLineName(5,2)
+L.SI_DQT_MAGES_GUILD				= GetSkillLineName(5,3)
+L.SI_DQT_UNDAUNTED_DELVE			= GetString(SI_VISUALARMORTYPE4).." "..GetString(SI_INSTANCEDISPLAYTYPE7)
 L.SI_DQT_TARNISHED					= "Tarnished"
 L.SI_DQT_BLACKFEATHER_COURT			= "Blackfeather Court"
 L.SI_DQT_RYES_REACQUISITIONS		= "Rye's Reacquisitions"
@@ -54,7 +54,7 @@ L.SI_DQT_ROOT_WHISPER				= "Root-Whisper"
 L.SI_DQT_NEW_MOON					= "New Moon" -- Added by DarkPhalanx
 L.SI_DQT_DRAGONHUNT					= "Dragon Hunts" -- Added by DarkPhalanx
 L.SI_DQT_HARROWSTORM				= "Harrowstorms" -- Added by DarkPhalanx
-L.SI_DQT_PVP						= "PVP" -- Added by DarkPhalanx
+L.SI_DQT_PVP						= GetString(SI_GUILDFOCUSATTRIBUTEVALUE5) -- Added by DarkPhalanx
 L.SI_DQT_RESISTANCE					= "Wayward Guardian" -- Updated by g4m3r7ag to match https://en.uesp.net/wiki/Online:Repeatable_Quests
 
 L.SI_DQT_CLOTHING					= GetString(SI_ITEMFILTERTYPE14)
@@ -72,24 +72,6 @@ any of these values from the beginning of each quest name.
 
 -- Undaunted Pledges
 L.SI_DQT_PLEDGE_PREFIX					= "Pledge: "
-
--- Undaunted Pledges. Urgarlag Chief-bane's Pledges
-L.SI_DQT_UNDAUNTED_PLEDGES_URGARLAG_CHIEF_BANE_S_PLEDGES_01 = "Pledge: Bloodroot Forge"
-L.SI_DQT_UNDAUNTED_PLEDGES_URGARLAG_CHIEF_BANE_S_PLEDGES_02 = "Pledge: Cradle of Shadows"
-L.SI_DQT_UNDAUNTED_PLEDGES_URGARLAG_CHIEF_BANE_S_PLEDGES_03 = "Pledge: Falkreath Hold"
-L.SI_DQT_UNDAUNTED_PLEDGES_URGARLAG_CHIEF_BANE_S_PLEDGES_04 = "Pledge: Fang Lair"
-L.SI_DQT_UNDAUNTED_PLEDGES_URGARLAG_CHIEF_BANE_S_PLEDGES_05 = "Pledge: Imperial City Prison"
-L.SI_DQT_UNDAUNTED_PLEDGES_URGARLAG_CHIEF_BANE_S_PLEDGES_06 = "Pledge: March of Sacrifices"
-L.SI_DQT_UNDAUNTED_PLEDGES_URGARLAG_CHIEF_BANE_S_PLEDGES_07 = "Pledge: Moon Hunter Keep"
-L.SI_DQT_UNDAUNTED_PLEDGES_URGARLAG_CHIEF_BANE_S_PLEDGES_08 = "Pledge: Ruins of Mazzatun"
-L.SI_DQT_UNDAUNTED_PLEDGES_URGARLAG_CHIEF_BANE_S_PLEDGES_09 = "Pledge: Scalecaller Peak"
-L.SI_DQT_UNDAUNTED_PLEDGES_URGARLAG_CHIEF_BANE_S_PLEDGES_10 = "Pledge: White-Gold Tower"
-L.SI_DQT_UNDAUNTED_PLEDGES_URGARLAG_CHIEF_BANE_S_PLEDGES_11 = "Pledge: Frostvault"
-L.SI_DQT_UNDAUNTED_PLEDGES_URGARLAG_CHIEF_BANE_S_PLEDGES_12 = "Pledge: Depths of Malatar"
-L.SI_DQT_UNDAUNTED_PLEDGES_URGARLAG_CHIEF_BANE_S_PLEDGES_13 = "Pledge: Lair of Maarselok"
-L.SI_DQT_UNDAUNTED_PLEDGES_URGARLAG_CHIEF_BANE_S_PLEDGES_14 = "Pledge: Moongrave Fane"
-L.SI_DQT_UNDAUNTED_PLEDGES_URGARLAG_CHIEF_BANE_S_PLEDGES_15 = "Pledge: Icereach"
-L.SI_DQT_UNDAUNTED_PLEDGES_URGARLAG_CHIEF_BANE_S_PLEDGES_16 = "Pledge: Unhallowed Grave"
 
 -- Vvardenfell Relics Quests
 L.SI_DQT_VVARDENFELL_QUESTS_RELICS_PREFIX_1	= "Relics of "
@@ -114,44 +96,50 @@ L.SI_DQT_THIEVES_GUILD_LARCENY_QUESTS_HEISTS_PREFIX_2 = "" -- placeholder for ot
 -- Dark Brotherhood Sacrament Quests
 L.SI_DQT_GOLD_COAST_QUESTS_DARK_BROTHERHOOD_SACRAMENTS_PREFIX				= "Sacrament: "
 
+-- Elsweyr Prologue Quests
+L.SI_DQT_NORTHERN_ELSWEYR_DEFENSE_FORCE_QUESTS_PREFIX_1 = "Dragon Lore: "
+L.SI_DQT_NORTHERN_ELSWEYR_DEFENSE_FORCE_QUESTS_PREFIX_2 = "" -- placeholder for other languages
+L.SI_DQT_NORTHERN_ELSWEYR_DEFENSE_FORCE_QUESTS_PREFIX_3 = "" -- placeholder for other languages
+L.SI_DQT_NORTHERN_ELSWEYR_DEFENSE_FORCE_QUESTS_PREFIX_4 = "" -- placeholder for other languages
+
 --[[ Alternate display names
 --]]
 -- Summerset Bounty Quests (World Boss)	
-L.SI_DQT_SUMMERSET_QUESTS_BOUNTY_01_DISPLAY		= "B'Korgen"
+--[[L.SI_DQT_SUMMERSET_QUESTS_BOUNTY_01_DISPLAY		= "B'Korgen"
 L.SI_DQT_SUMMERSET_QUESTS_BOUNTY_02_DISPLAY		= "Gryphons"
 L.SI_DQT_SUMMERSET_QUESTS_BOUNTY_03_DISPLAY		= "Graveld"
 L.SI_DQT_SUMMERSET_QUESTS_BOUNTY_04_DISPLAY		= "Keelsplitter"
 L.SI_DQT_SUMMERSET_QUESTS_BOUNTY_05_DISPLAY		= "Queen of the Reef"
-L.SI_DQT_SUMMERSET_QUESTS_BOUNTY_06_DISPLAY		= "Caanerin"
+L.SI_DQT_SUMMERSET_QUESTS_BOUNTY_06_DISPLAY		= "Caanerin"--]]
 
 -- Vvardenfell Bounty Quests (World Boss)	
-L.SI_DQT_VVARDENFELL_QUESTS_BOUNTY_01_DISPLAY	= "Dubdil Alar"
+--[[L.SI_DQT_VVARDENFELL_QUESTS_BOUNTY_01_DISPLAY	= "Dubdil Alar"
 L.SI_DQT_VVARDENFELL_QUESTS_BOUNTY_02_DISPLAY	= "Wuyuvus"
 L.SI_DQT_VVARDENFELL_QUESTS_BOUNTY_03_DISPLAY	= "Queen's Consort"
 L.SI_DQT_VVARDENFELL_QUESTS_BOUNTY_04_DISPLAY	= "Nilthog the Unbroken"
 L.SI_DQT_VVARDENFELL_QUESTS_BOUNTY_05_DISPLAY	= "Orator Salothan"
-L.SI_DQT_VVARDENFELL_QUESTS_BOUNTY_06_DISPLAY	= "Kimbrudhil the Songbird"
+L.SI_DQT_VVARDENFELL_QUESTS_BOUNTY_06_DISPLAY	= "Kimbrudhil the Songbird"--]]
 
 -- Wrothgar Group Boss Quests	
-L.SI_DQT_WROTHGAR_QUESTS_GROUP_BOSS_DAILIES_01_DISPLAY	= "Zandadunoz the Reborn"
+--[[L.SI_DQT_WROTHGAR_QUESTS_GROUP_BOSS_DAILIES_01_DISPLAY	= "Zandadunoz the Reborn"
 L.SI_DQT_WROTHGAR_QUESTS_GROUP_BOSS_DAILIES_02_DISPLAY	= "Snagara"
 L.SI_DQT_WROTHGAR_QUESTS_GROUP_BOSS_DAILIES_03_DISPLAY	= "Corintthac the Abomination"
 L.SI_DQT_WROTHGAR_QUESTS_GROUP_BOSS_DAILIES_04_DISPLAY	= "King-Chief Edu"
 L.SI_DQT_WROTHGAR_QUESTS_GROUP_BOSS_DAILIES_05_DISPLAY	= "Mad Urkazbur"
-L.SI_DQT_WROTHGAR_QUESTS_GROUP_BOSS_DAILIES_06_DISPLAY	= "Nyzchaleft"
+L.SI_DQT_WROTHGAR_QUESTS_GROUP_BOSS_DAILIES_06_DISPLAY	= "Nyzchaleft"--]]
 
 -- Dark Brotherhood Bounty Quests
-L.SI_DQT_GOLD_COAST_QUESTS_BOUNTIES_01_DISPLAY	= "Exulus the Wispmother"
+--[[L.SI_DQT_GOLD_COAST_QUESTS_BOUNTIES_01_DISPLAY	= "Exulus the Wispmother"
 L.SI_DQT_GOLD_COAST_QUESTS_BOUNTIES_02_DISPLAY	= "Ironfang"
 L.SI_DQT_GOLD_COAST_QUESTS_BOUNTIES_03_DISPLAY	= "Limenauruus"
-L.SI_DQT_GOLD_COAST_QUESTS_BOUNTIES_04_DISPLAY	= "The Roar of the Crowds"
+L.SI_DQT_GOLD_COAST_QUESTS_BOUNTIES_04_DISPLAY	= "The Roar of the Crowds"--]]
 
 -- Clockwork City Bounty Quests
-L.SI_DQT_CLOCKWORK_CITY_QUESTS_BOUNTY_01_DISPLAY = "Wraith-of-Crows"
-L.SI_DQT_CLOCKWORK_CITY_QUESTS_BOUNTY_02_DISPLAY = "Imperfect"
+--[[L.SI_DQT_CLOCKWORK_CITY_QUESTS_BOUNTY_01_DISPLAY = "Wraith-of-Crows"
+L.SI_DQT_CLOCKWORK_CITY_QUESTS_BOUNTY_02_DISPLAY = "Imperfect"--]]
 
 -- Northern Elsweyr Defense Force (quest names are a bit too long for the gui)
-L.SI_DQT_NORTHERN_ELSWEYR_DEFENSE_FORCE_01_DISPLAY = "Dark Souls"
+--[[L.SI_DQT_NORTHERN_ELSWEYR_DEFENSE_FORCE_01_DISPLAY = "Dark Souls"
 L.SI_DQT_NORTHERN_ELSWEYR_DEFENSE_FORCE_02_DISPLAY = "Icehammer's Vault"
 L.SI_DQT_NORTHERN_ELSWEYR_DEFENSE_FORCE_03_DISPLAY = "Shroud Hearth"
 L.SI_DQT_NORTHERN_ELSWEYR_DEFENSE_FORCE_04_DISPLAY = "Stormcrag Crypt"
@@ -159,7 +147,7 @@ L.SI_DQT_NORTHERN_ELSWEYR_DEFENSE_FORCE_05_DISPLAY = "Goblin"
 L.SI_DQT_NORTHERN_ELSWEYR_DEFENSE_FORCE_06_DISPLAY = "Lamia"
 L.SI_DQT_NORTHERN_ELSWEYR_DEFENSE_FORCE_07_DISPLAY = "Lurcher"
 L.SI_DQT_NORTHERN_ELSWEYR_DEFENSE_FORCE_08_DISPLAY = "Skeleton"
-L.SI_DQT_NORTHERN_ELSWEYR_DEFENSE_FORCE_09_DISPLAY = "Spider"
+L.SI_DQT_NORTHERN_ELSWEYR_DEFENSE_FORCE_09_DISPLAY = "Spider"--]]
 
 for stringId, translation in pairs(L) do
 	-- In other language files, use SafeAddString instead, e.g. SafeAddString(_G[stringId], translation, 0)
